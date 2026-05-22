e1666a42328c6c7f32b4c78b83d0d4a2d579a908
nguyentr02 <truongphamtuannguyen@gmail.com>
2026-05-22 16:38:12 +0700

    fix: serve swagger ui via cdn for vercel compatibility

--- src/app.js
+++ src/app.js
@@ -1,21 +1,52 @@
+require("dotenv").config();
 const express = require("express");
-const { swaggerUi, specs } = require("./swagger");
-const dotenv = require("dotenv");
-const { testConnection } = require("./config/db.js");
-const { initialize } = require("./database.js");
+const { specs } = require("./swagger");
+const { initialize } = require("./database");
 const studentRoutes = require("./routes/students");
 const subjectRoutes = require("./routes/subjects.js");
 const classRoutes = require("./routes/class.js");
 const enrollRoutes = require("./routes/enroll.js");
 
-dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-console.log("App.js received specs:", !!specs);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+// Serve swagger spec as JSON
+app.get("/api-docs/swagger.json", (req, res) => {
+  res.json(specs);
+});
+
+// Serve Swagger UI via CDN
+app.get("/api-docs", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>UMS API Docs</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" >
+      </head>
+      <body>
+        <div id="swagger-ui"></div>
+        <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"> </script>
+        <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js"> </script>
+        <script>
+          window.onload = function() {
+            SwaggerUIBundle({
+              url: "/api-docs/swagger.json",
+              dom_id: '#swagger-ui',
+              presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+              layout: "StandaloneLayout"
+            })
+          }
+        </script>
+      </body>
+    </html>
+  `);
+});
+
 app.use("/student", studentRoutes);
 app.use("/subject", subjectRoutes);
 app.use("/class", classRoutes);
@@ -25,12 +56,12 @@ app.get("/", (req, res) => {
   res.json({ message: "API is Running" });
 });
 
-const start = async () => {
+async function start() {
   await initialize();
   app.listen(PORT, () => {
     console.log(` Server running on http://localhost:${PORT}`);
     console.log(`API Docs available at http://localhost:${PORT}/api-docs`);
   });
-};
+}
 
 start();
